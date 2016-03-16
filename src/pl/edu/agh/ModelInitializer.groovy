e7db052281a415d4aa0e71c7ecd2c02fc2c5c1d1
Hoobie <hoobie92@gmail.com>
2016-03-16 19:22:28 +0100

    Initial commit

---
+++ src/pl/edu/agh/ModelInitializer.groovy
@@ -0,0 +1,64 @@
+package pl.edu.agh
+
+import java.math.*
+import javax.measure.unit.*
+import org.jscience.mathematics.number.*
+import org.jscience.mathematics.vector.*
+import org.jscience.physics.amount.*
+import repast.simphony.adaptation.neural.*
+import repast.simphony.adaptation.regression.*
+import repast.simphony.context.*
+import repast.simphony.context.space.continuous.*
+import repast.simphony.context.space.gis.*
+import repast.simphony.context.space.graph.*
+import repast.simphony.context.space.grid.*
+import repast.simphony.engine.environment.*
+import repast.simphony.engine.schedule.*
+import repast.simphony.engine.watcher.*
+import repast.simphony.groovy.math.*
+import repast.simphony.integration.*
+import repast.simphony.matlab.link.*
+import repast.simphony.query.*
+import repast.simphony.query.space.continuous.*
+import repast.simphony.query.space.gis.*
+import repast.simphony.query.space.graph.*
+import repast.simphony.query.space.grid.*
+import repast.simphony.query.space.projection.*
+import repast.simphony.parameter.*
+import repast.simphony.random.*
+import repast.simphony.space.continuous.*
+import repast.simphony.space.gis.*
+import repast.simphony.space.graph.*
+import repast.simphony.space.grid.*
+import repast.simphony.space.projection.*
+import repast.simphony.ui.probe.*
+import repast.simphony.util.*
+import simphony.util.messages.*
+import static java.lang.Math.*
+import static repast.simphony.essentials.RepastEssentials.*
+
+public class ModelInitializer  {
+
+    private static final long serialVersionUID = 1L
+	
+    protected static long agentIDCounter = 1
+	
+    protected String agentID = "ModelInitializer " + (agentIDCounter++)
+    
+    public def initializeModel() {
+        def returnValue
+        def time = GetTickCountInTimeUnits()
+
+        return returnValue
+    }
+
+    @ProbeID()
+    public String toString() {
+        def returnValue
+        def time = GetTickCountInTimeUnits()
+
+        returnValue = this.agentID
+
+		return returnValue
+    }
+}
