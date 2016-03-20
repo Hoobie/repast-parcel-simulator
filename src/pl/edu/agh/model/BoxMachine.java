9a4ae1546f8812058f5ba858718454257f861da0
Hoobie <hoobie92@gmail.com>
2016-03-21 00:49:04 +0100

    Add basic context

---
+++ src/pl/edu/agh/model/BoxMachine.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.model;
+
+import repast.simphony.space.gis.Geography;
+
+public class BoxMachine {
+
+	private Geography<Object> geography;
+
+	public BoxMachine(Geography<Object> geography) {
+		this.geography = geography;
+	}
+}
