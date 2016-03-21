fe705e0727d5f3d073104cacd4f61d4be3b86105
Hoobie <hoobie92@gmail.com>
2016-03-21 22:22:49 +0100

    Add shapefiles handling

--- src/pl/edu/agh/ContextInitializer.java
+++ src/pl/edu/agh/ContextInitializer.java
@@ -1,5 +1,7 @@
 package pl.edu.agh;
 
+import java.io.File;
+
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
 
@@ -10,24 +12,33 @@ import repast.simphony.context.space.gis.GeographyFactoryFinder;
 import repast.simphony.dataLoader.ContextBuilder;
 import repast.simphony.space.gis.Geography;
 import repast.simphony.space.gis.GeographyParameters;
+import repast.simphony.space.gis.ShapefileLoader;
 
 public class ContextInitializer implements ContextBuilder<Object> {
 
 	@Override
 	public Context<Object> build(Context<Object> context) {
 		context.setId("parcel-simulator");
-		
+
 		GeographyParameters<Object> params = new GeographyParameters<Object>();
-		//params.setAdder(new RandomGisAdder<Object>());
-		GeographyFactory factory = GeographyFactoryFinder.createGeographyFactory(null);
-		Geography<Object> geography = factory.createGeography("geography", context, params);
-		
-		for (int i = 0; i < 10; i++) {
-			BoxMachine bm = new BoxMachine(geography);
-			context.add(bm);
-			geography.move(bm, new GeometryFactory().createPoint(new Coordinate(i, i + 1)));
+		GeographyFactory factory = GeographyFactoryFinder
+				.createGeographyFactory(null);
+		Geography<Object> geography = factory.createGeography("geography",
+				context, params);
+
+		File shapefile = null;
+		ShapefileLoader<BoxMachine> loader = null;
+		try {
+			shapefile = new File("misc/shp/boxmachines/boxmachines.shp");
+			loader = new ShapefileLoader<BoxMachine>(BoxMachine.class,
+					shapefile.toURL(), geography, context);
+		} catch (java.net.MalformedURLException e) {
+			e.printStackTrace();
 		}
-		
+		while (loader.hasNext()) {
+			loader.next();
+		}
+
 		return context;
 	}
 
